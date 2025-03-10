f828c93fdc9eb37cd973a9d4bf6f6990bb8ee558
ChrisvdBerg5 <C.vandenBerg-3@student.tudelft.nl>
2025-03-10 10:25:25 +0100

    Update processing.py

--- Processing/processing.py
+++ Processing/processing.py
@@ -1,1 +1,11 @@
-eecbsdcd
+import numpy as np
+
+def load_data(files):
+    data = np.load(files)
+    
+    extracted_data = {name: data[name] for name in data.files}
+    
+    return extracted_data
+
+
+print(load_data(r"C:\\Users\\chris\\OneDrive\\Documenten\\Chris\\Q3 2024-2025\\Project\\stockv_subsampled_train_perc_10.npz"))
