ef2b85c43fff5a797d1ac47f3bb7f761c8aaf983
Sofia Calvo Morales <sofiacalvomorales@gmail.com>
2025-03-10 10:31:08 +0100

    First commit for FID document
    Addition and explanaition of how it will be done

---
+++ Validation/FID.py
@@ -0,0 +1,5 @@
+## FID is a method that calculates the distance between distribution of datapoints.
+## Since out data is high dimensional, for each time step, one feature of each trajectory will be taking
+## This will result in distributions of real and synthetic data, one for each datapoint and feature
+## Finally, the FID is calculated for each of these distributions, obtaining a FID score in time for each feature
+
