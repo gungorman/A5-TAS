b6a9bf4c7fcc49eeb94d68b7a70a1b1aade73d2f
Sofia Calvo Morales <sofiacalvomorales@gmail.com>
2025-03-17 13:49:13 +0100

    Update FID.py

--- Validation/FID.py
+++ Validation/FID.py
@@ -4,6 +4,7 @@
 ## Finally, the FID is calculated for each of these distributions, obtaining a FID score in time for each feature
 
 import numpy as np
+import matplotlib.pyplot as plt
 
 ## Import the data and take just the first 4 columns, removes the title column
 data = np.genfromtxt('C:/Sofia/TU Delft/Bsc2/Proyect/second semester/EHAM_LIMC.csv', delimiter=',')
@@ -15,20 +16,31 @@ def FID(real_data, synth_data):
     synth_data = np.delete(synth_data, 0, 0)
     synth_data = np.delete(synth_data, np.s_[4:], 1)
 
-    ## look for starts
+    ## look for start of trajectories
     real_index = []
     synth_index = []
     for i in range(len(real_data)):
         if real_data[i][3] == 0:
             real_index.append(i)
-#    for i in range(len(synth_data)):
-#        if synth_data[i][3] == 0:
-#            synth_index.append(i)
-    flight_size = real_index[1]
-    alt_real_mean = [] ## List for storing av altitude in real data
-    alt_real_cov = [] ## List for storing covariance of altitude of real data
-    alt_real = real_data[i:, 2] ## List with all real altitudes
-    print(alt_real)
-    for i in range(flight_size):
-        alt_dt = alt_real[i::flight_size]
+    for i in range(len(synth_data)):
+        if synth_data[i][3] == 0:
+            synth_index.append(i)
+    flight_size = real_index[1] ## This assumes all trajectories have the same size, which they do in this experiment 
+
+    # Lists to store values of average and covariance
+    real_mean = [] ## List for storing av in real data features
+    real_cov = [] ## List for storing covariance of real data features
+    synth_mean = [] ## Same for synth
+    synth_mean = [] ## Same for synth
+
+    # For each datapoint, obtain the mean a covariance for real and synth trajectories. Store the values in the lists
+    for datapoint in range(flight_size):
+        real_dtp = real_data[datapoint::flight_size]
+        synth_dtp = synth_data[datapoint::flight_size]
+        real_means_dtp = []
+        covariance = []
+        for i in range(len(real_dtp[0])):
+            real_means_dtp = np.append(real_means_dtp, np.mean(real_dtp[:, i]))
+        real_mean = np.append(real_mean, real_means_dtp)
+    print(real_mean) ## To do, fix means to be in separate rows in the general array
 FID(data, data)
