15710c7f9b0f5732620c3868a5bbd0ab8fa32110
gungorman <gungoremre2004@gmail.com>
2025-03-27 10:01:52 +0100

    Update processing-emre.py

--- Processing/processing-emre.py
+++ Processing/processing-emre.py
@@ -1,24 +1,6 @@
 import pandas as pd
 import numpy as np
 
-
-def load_data(files):
-
-
-    data = pd.read_csv(files)
-    #data.drop(columns=["callsign", "icao24", "cluster", "timestamp"], inplace=True)
-    x = data['latitude'].to_numpy()
-    y = data['longitude'].to_numpy()
-    z = data['altitude'].to_numpy()
-    t = data['timedelta'].to_numpy()
-    f = data['flight_id'].to_numpy()
-
-    # Combine the arrays into a single numpy array
-    #combined_array = np.column_stack((x, y, z, t))
-    #print(load_data(r"C:\Users\gungo\Downloads\ESSA_LFPG.csv"))
-    return combined_array
-
-
 def number_of_flights(files):
 
 # Assuming 'files' is the path to your CSV file
